db0e2f21c1e563279a0756aa2f8e387fb6f874f2
José Nieto <jdnv.123@gmail.com>
2018-03-13 17:12:09 -0500

    Style banner and signin form

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -13,13 +13,14 @@ export default class HomeBanner extends Component {
         return (
             <div style={ bannerStyle } class="home banner">
                 <div className="ui grid">
-                    <div className="eight wide column">
+                    <div className="eleven wide column">
                         <div className="ui container">
                             <h1>Á g o r a</h1>
+                            <div className="ui divider"></div>
                             <Searchbar />
                         </div>
                     </div>
-                    <div className="eight wide column">
+                    <div id="" className="five wide left column">
                         <SigninForm />
                     </div>
                 </div> 
--- src/components/SigninForm.js
+++ src/components/SigninForm.js
@@ -4,12 +4,12 @@ import './SigninForm.css';
 export default class SigninForm extends Component {
     render() {
         return (
-            <div class="ui signin padded segment">
+            <div className="ui signin container">
                 <h3 className="ui centered header">
                     Inicia sesión en Ágora
                 </h3>
                 <div className="ui divider"></div>
-                <form class="ui form">
+                <form class="ui signin form">
                     <div class="field">
                         <input name="email" placeholder="Correo electrónico" type="text" />
                     </div>
@@ -19,7 +19,7 @@ export default class SigninForm extends Component {
                     <h4 className="ui centered header">
                         Si no tienes una cuenta, <a href="#">regístrate ahora</a>.
                     </h4>
-                    <button class="ui green button" type="submit">Iniciar sesión</button>
+                    <button class="ui green inverted button" type="submit">Iniciar sesión</button>
                 </form>
             </div>
         );
