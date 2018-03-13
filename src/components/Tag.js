d615b5f2a63d9b2d714992f37f18798e28b111e6
José Nieto <jdnv.123@gmail.com>
2018-03-13 15:31:27 -0500

    Add Tag dynamic component

---
+++ src/components/Tag.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ImgCapitolio from '../images/capitolio.png';
+
+export default class Tag extends Component {
+    render() {
+        return (
+            <div className="ui category card">
+                <div className="image">
+                    <img src={ ImgCapitolio } />
+                </div>
+                <div className="content">
+                    <a href="#" className="header">{this.props.title}</a>
+                </div>
+            </div>
+        );
+    }
+}
--- src/components/TagCarousel.js
+++ src/components/TagCarousel.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import Tag from './Tag.js'
 
 export default class TagCarousel extends Component {
+    componentWillMount() {
+
+    }
+
     render() {
+        // obtener de la api
+        
         return (
             <div className="ui tag carousel segment">
                 <h2>Tag Carousel</h2>
+                <div className="ui cards">
+                    <Tag title="Corrupción"/>
+                    <Tag title="Elecciones"/>
+                </div>
             </div>
         );
     }
