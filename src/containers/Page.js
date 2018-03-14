0841df55b94e04b528350d4896783fe8e6ea83d9
José Nieto <jdnv.123@gmail.com>
2018-03-13 19:37:22 -0500

    Add routing, create Page container

---
+++ src/containers/Page.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import HomePage from './HomePage.js';
+import { Switch, Route } from 'react-router-dom';
+
+export default Page = () => {
+    <main>
+        <Switch>
+            <Router exact path='/' component={HomePage}/>
+        </Switch>
+    </main>
+}
