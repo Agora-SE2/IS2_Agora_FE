364d9c93eab759a10a59272c18f52ba1066e996e
José Nieto <jdnietov@unal.edu.co>
2018-03-13 20:16:49 -0500

    Hotfix: even more className

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Navbar extends Component {
     render() {
         return (
-            <div id="navbar" class="ui top fixed big menu">
+            <div id="navbar" className="ui top fixed big menu">
                 <div className="ui container">
                     <a href="/" className="active item">
                         Ágora
@@ -14,13 +14,13 @@ export default class Navbar extends Component {
                     <a href="/about" className="item">
                         Nosotros
                     </a>
-                    <div class="right menu">
-                        <div class="ui category search item">
-                            <div class="ui icon input">
-                                <input class="prompt" placeholder="Busca proyectos de ley..." type="text"/>
-                                <i class="search link icon"></i>
+                    <div className="right menu">
+                        <div className="ui category search item">
+                            <div className="ui icon input">
+                                <input className="prompt" placeholder="Busca proyectos de ley..." type="text"/>
+                                <i className="search link icon"></i>
                             </div>
-                            <div class="results"></div>
+                            <div className="results"></div>
                         </div>
                     </div>
                 </div>                
