3e7596ccf9c897dd9860c87b8b97c99effd40fca
José Nieto <jdnv.123@gmail.com>
2018-03-13 19:52:18 -0500

    Route URLs to page set

--- src/containers/Page.js
+++ src/containers/Page.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import HomePage from './HomePage.js';
 import { Switch, Route } from 'react-router-dom';
 
-export default Page = () => {
-    <main>
-        <Switch>
-            <Router exact path='/' component={HomePage}/>
-        </Switch>
-    </main>
+import HomePage from './HomePage.js';
+import AboutPage from './AboutPage.js';
+import CategoryPage from './CategoryPage.js';
+import ProfilePage from './ProfilePage.js';
+import SearchPage from './SearchPage.js';
+import ProyectoLeyPage from './ProyectoLeyPage.js';
+
+import './Page.css';
+
+export default class Page extends Component {
+    render() {
+        return (
+            <main>
+                <Switch>
+                    <Route exact path='/' component={HomePage}/>
+                    <Route exact path='/about' component={AboutPage}/>
+                    <Route path='/proyectoley' component={ProyectoLeyPage}/>
+                    <Route path='/categoria' component={CategoryPage}/>
+                    <Route path='/search' component={SearchPage}/>
+                    <Route path='/profile' component={ProfilePage}/>
+                </Switch>
+            </main>
+        );
+    }
 }
---
+++ src/containers/ProyectoLeyPage.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class ProyectoLeyPage extends Component {
+    render() {
+        return (
+            <h1>ProyectoLeyPage</h1>
+        );
+    }
+}
