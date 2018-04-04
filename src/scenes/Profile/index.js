b7d099f811c6e51315826eb2fd72d091295573d9
José Nieto <jdnietov@unal.edu.co>
2018-04-04 01:21:06 -0500

    Initialize profile view

--- src/scenes/Profile/index.js
+++ src/scenes/Profile/index.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 
+import Humberto from 'images/humberto.jpg';
+
+import { Header, Image} from 'semantic-ui-react';
+
 export default class Profile extends Component {
     render() {
         return (
-            <h1>ProfilePage</h1>
+            <div className="ui page container">
+                <Image centered circular size="medium" src={Humberto} />
+                <Header textAlign="center" as="h1">
+                    {' '}Humberto de la Calle
+                    <Header.Subheader>Usuario</Header.Subheader>
+                </Header>
+            </div>
         );
     }
 }
