9744e4b5a239b5bd95035338e497ac492fb2bafa
José Nieto <jdnietov@unal.edu.co>
2018-04-04 23:11:15 -0500

    Add Redux

---
+++ src/components/CommentList/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+//import { Comment } from 'semantic-ui-react';
+
+import AgoraComment from './components/Comment/index.js';
+import './styles.css';
+
+export default class Comments extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
