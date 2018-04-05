c00072f78b5e37e43a318652a9ae48fc51b3ccb6
Ruben Buelvas <rcbuelvasv@gmail.com>
2018-04-04 22:51:56 -0500

    User Option Navbar

--- src/components/Navbar/components/UserOptions/index.js
+++ src/components/Navbar/components/UserOptions/index.js
@@ -1,7 +1,23 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
+import Humberto from '../../../../images/humberto.jpg'
 
-// export default class UserOptions extends Component {
-//     render() {
-//         return ();
-//     }
-// }
+export default class UserOptions extends Component {
+    render() {
+        return (
+        <div className="right menu">
+            <div className="ui category search item">
+                <div className="ui icon input">
+                    <input className="prompt" placeholder="Busca proyectos de ley..." type="text"/>
+                    <i className="search link icon"></i>
+                </div>
+                <div className="results"></div>
+            </div>
+            <div className="item">
+                <img className="ui avatar image" src={Humberto}/>
+                <span>Humberto de la Calle</span>
+        
+            </div>
+            </div>  
+        );
+    }
+}
