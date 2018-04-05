4a7a9a3c1bce6a903b3f786e3128aea94b2a9e8a
José Nieto <jdnietov@unal.edu.co>
2018-04-05 04:25:10 -0500

    Fix ProjectCard to use TagLabelList

--- src/scenes/Search/components/ProjectCard/index.js
+++ src/scenes/Search/components/ProjectCard/index.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
 import ProjectImage from '../../../../images/economia.jpeg';
-import TagLabel from 'components/TagLabel/index.js';
+import TagLabelList from 'components/TagLabelList/index.js';
 
 const tags = [{id:1, name:"Economía"}, {id:2, name:"JEP"}];
-        let tagViews = [];
-        for(const tag of tags) {
-            tagViews.push(<TagLabel key={tag.id.toString()} id={tag.id} name={tag.name} />);
-        }
 
 export default class ProjectCard extends Component {
     render() {
@@ -23,9 +19,7 @@ export default class ProjectCard extends Component {
                             <div className="description">
                                 <p>Whatever</p>
                             </div>
-                            <div className="tags">
-                                {tagViews}
-                            </div>
+                            <TagLabelList tags={tags} />
                         </div>
                     </div>
                 </div>
