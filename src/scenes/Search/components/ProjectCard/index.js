0d56a96d4fdcc4d310b0e2e52a41262eb13d408d
Ruben Buelvas <rcbuelvasv@gmail.com>
2018-04-05 01:09:11 -0500

    Search results cards alpha

--- src/scenes/Search/components/ProjectCard/index.js
+++ src/scenes/Search/components/ProjectCard/index.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import ProjectImage from '../../../../images/economia.jpeg';
+import TagLabel from 'components/TagLabel/index.js';
+
+const tags = [{id:1, name:"Economía"}, {id:2, name:"JEP"}];
+        let tagViews = [];
+        for(const tag of tags) {
+            tagViews.push(<TagLabel key={tag.id.toString()} id={tag.id} name={tag.name} />);
+        }
 
 export default class ProjectCard extends Component {
     render() {
         return (
             <div className="ui project segment">
-                CNZJVCZXJVNJK
+                <div className="ui items">
+                    <div className="item">
+                        <div className="image">
+                            <img src={ProjectImage}/>
+                        </div>
+                        <div className="content">
+                            <a className="header">Proyecto #1</a>
+                            <hr/>
+                            <div className="description">
+                                <p>Whatever</p>
+                            </div>
+                            <div className="tags">
+                                {tagViews}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
