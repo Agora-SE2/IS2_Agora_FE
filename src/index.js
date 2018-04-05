a8dc048d502b4d454fb932102a17c3785889cdea
José Nieto <jdnietov@unal.edu.co>
2018-04-05 14:04:36 -0500

    Install redux, declare reducer

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
 
 import { BrowserRouter } from 'react-router-dom';
 
-import Navbar from './components/Navbar/index.js';
-import Footer from './components/Footer/index.js';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
-import About from './scenes/About/index.js';
-import Arguments from './scenes/Arguments/index.js';
+import About from './scenes/About';
+import Arguments from './scenes/Arguments';
 import Category from './scenes/Category/index.js';
 import Home from './scenes/Home/index.js';
 import LawProject from './scenes/LawProject/index.js';
@@ -17,27 +18,30 @@ import Profile from './scenes/Profile/index.js';
 import Search from './scenes/Search/index.js';
 import Signup from './scenes/Signup/index.js';
 
+import store from './store/index.js';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 
 ReactDOM.render((
-    <BrowserRouter>
-      <div id="app">
-        <Navbar />
-        <main>
-          <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route exact path='/about' component={About}/>
-            <Route exact path='/proyectoley:id/args' component={Arguments} />
-            <Route path='/proyectoley/:id' component={LawProject} />
-            <Route path='/categoria/:name' component={Category}/>
-            <Route path='/login' component={Login}/>
-            <Route path='/search' component={Search}/>
-            <Route path='/profile/:id' component={Profile}/>
-            <Route path='/signup' component={Signup}/>
-          </Switch>
-        </main>
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div id="app">
+          <Navbar />
+          <main>
+            <Switch>
+              <Route exact path='/' component={Home}/>
+              <Route exact path='/about' component={About}/>
+              <Route exact path='/proyectoley:id/args' component={Arguments} />
+              <Route path='/proyectoley/:id' component={LawProject} />
+              <Route path='/categoria/:name' component={Category}/>
+              <Route path='/login' component={Login}/>
+              <Route path='/search' component={Search}/>
+              <Route path='/profile/:id' component={Profile}/>
+              <Route path='/signup' component={Signup}/>
+            </Switch>
+          </main>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </Provider>
 ), document.getElementById('root'));
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,16 @@
-const initialState = {
-    articles: []
-  };
-  const rootReducer = (state = initialState, action) => state;
-  export default rootReducer;
+const rootReducer = (state = {user: {}}, action) => {
+  switch(action.type) {
+    case "LOGIN":
+      state.user = action.payload;
+      return state;
+
+    case "LOGOUT":
+      state.user = {}
+      return state;
+      
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
