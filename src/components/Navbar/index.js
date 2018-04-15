4b9b01d6e9311d5ff59079f30916b5d05ada9820
Ruben Buelvas <rcbuelvasv@gmail.com>
2018-04-14 20:59:43 -0500

    Navbar and colors scss

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,6 +5,8 @@ import logo from './images/logo.svg';
 import VisitorOptions from './components/VisitorOptions/index.js';
 import UserOptions from './components/UserOptions/index.js';
 
+import './styles.css';
+
 @connect((store) => {
     return {
         token: store.user.token
@@ -21,18 +23,17 @@ export default class Navbar extends Component {
         }
 
         return (
-            <div id="navbar" className="ui top fixed big menu">
-                <a href="/" className="active item">
-                    <img src={logo} alt="Logo" />
-                </a>
-                <a href="/search" className="item">
-                    Proyectos de ley
-                </a>
-                <a href="/about" className="item">
-                    Nosotros
-                </a>
-                {options}
-            </div>
+            <div id="navbar" className="ui top fixed secondary menu"> 
+                <div className="ui container"> 
+                    <a id="logo" href="/" className="active item"> 
+                        <img src={logo} alt="Logo" /> 
+                    </a> 
+                    <a href="/search" className="item"> 
+                        Proyectos de ley 
+                    </a> 
+                    {options} 
+                </div> 
+            </div> 
         );
     }
 }
