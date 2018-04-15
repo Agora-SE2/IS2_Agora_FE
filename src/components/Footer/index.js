19c0f194f3482f78241b473fc5228c8c5d579150
Ruben Buelvas <rcbuelvasv@gmail.com>
2018-04-14 22:34:12 -0500

    Footer link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,7 @@ export default class Footer extends Component {
     render() {
         return (
             <div id="footer">
-                <p>Ágora (2018) - un proyecto por Los Investigados</p>
+                <p>Ágora (2018) - un proyecto por <a href="/About">Los Investigados</a></p>
             </div>
         );        
     }
