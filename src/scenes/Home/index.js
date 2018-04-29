9164086413740af720f7ad775ab61d6807836d38
José Nieto <jdnietov@unal.edu.co>
2018-04-29 17:27:31 -0500

    Install CanvasJS

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import HomeAdmin from './components/HomeAdmin';
 import HomeUser from './components/HomeUser';
 import HomeVisitor from './components/HomeVisitor';
 
 @connect((store) => {
     return {
+        isAdmin: store.currentUser.isAdmin,
         loggedIn: store.loggedIn
     };
 })
 export default class Home extends Component {
     render() {
         if(this.props.loggedIn)
-            return <HomeUser />;   // TODO: not really
+            if(this.props.isAdmin)
+                return <HomeAdmin />;
+            else
+                return <HomeUser />;
         else
             return <HomeVisitor />;
     }
