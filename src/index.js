f5b76bd7531465fe22dfc7d62847de1268a6bff0
Ruben Buelvas <rcbuelvasv@gmail.com>
2018-05-03 23:13:52 -0500

    Cors test

--- src/index.js
+++ src/index.js
@@ -27,6 +27,20 @@ import PDFView from './scenes/PDFView';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 
+var express = require('express')
+var cors = require('cors')
+var app = express()
+ 
+app.use(cors())
+ 
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+ 
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
+
 ReactDOM.render((
     <Provider store={store}>
       <PersistGate loading={(() => <div>Loading</div>)()} persistor={persistor}>
